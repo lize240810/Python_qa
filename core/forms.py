814524ae7d7102cf4fa19eff87ed867e0d6637e7
lize <liz240810e@163.com>
2018-11-27 17:49:17 +0800

    界面美化

--- core/forms.py
+++ core/forms.py
@@ -3,8 +3,9 @@ from django.contrib.auth import get_user_model
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
+    password = forms.CharField(widget=forms.PasswordInput(), label='密码')
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), label='确认密码')
+    
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'password')
